05ef875504d554b4e039e513a7789f89ec443894
George Gale <gmgale@icloud.com>
2024-06-27 23:13:26 +0200

    update gradle conf

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.george.gale"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -54,5 +54,10 @@ tasks {
 
     buildSearchableOptions {
         enabled = false
+    }
+
+    processResources {
+        includeEmptyDirs = false
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     }
 }
